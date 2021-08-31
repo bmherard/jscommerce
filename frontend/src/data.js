625afe025aeb6bccce9b15818a3baf381fa35067
Brian Herard <bmherard@gmail.com>
2021-08-31 16:24:17 -0400

    Initial Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Full Zip Running Shirt',
+            category: 'Althletic Shirts',
+            image: '/images/product-1.jpg',
+            price: 59.97,
+            brand: 'Spor-tee',
+            rating: '4.5',
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'Half Zip Running Shirt',
+            category: 'Althletic Shirts',
+            image: '/images/product-2.jpg',
+            price: 49.97,
+            brand: 'Spor-tee',
+            rating: '4.0',
+            numReviews: 7,
+            countInStock: 12
+        },
+        {
+            _id: '3',
+            name: 'Running Pants',
+            category: 'Althletic Pants',
+            image: '/images/product-3.jpg',
+            price: 69.97,
+            brand: 'Spor-tee',
+            rating: '4.5',
+            numReviews: 9,
+            countInStock: 5
+        },
+        {
+            _id: '4',
+            name: 'Running Shorts',
+            category: 'Althletic Shorts',
+            image: '/images/product-4.jpg',
+            price: 39.97,
+            brand: 'Spor-tee',
+            rating: '3.5',
+            numReviews: 20,
+            countInStock: 15
+        },
+        {
+            _id: '5',
+            name: 'Running Flat',
+            category: 'Althletic Shoes',
+            image: '/images/product-5.jpg',
+            price: 94.97,
+            brand: 'Spor-tee',
+            rating: '4.0',
+            numReviews: 21,
+            countInStock: 7
+        },
+        {
+            _id: '6',
+            name: 'Running Shoe',
+            category: 'Althletic Shoes',
+            image: '/images/product-6.jpg',
+            price: 129.97,
+            brand: 'Spor-tee',
+            rating: '5',
+            numReviews: 50,
+            countInStock: 19
+        },
+    ],
+};
